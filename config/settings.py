e0f18b77c67c6944c1e8ef5c449d5ba03ad3bc5c
Deiker Diaz <deiker21diaz@gmail.com>
2023-01-15 22:30:56 -0400

    Change debug to true

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
